35b9e054fb75782f53712680b83fde52facbf50a
Reuben-Lawton <85323219+Reuben-Lawton@users.noreply.github.com>
2022-04-25 01:58:44 +1000

    started messing around with brief 03

---
+++ Assets/Scripts/PickupsScripts/Pickups.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pickups : MonoBehaviour
+{
+    [Header("In Game Pickups")]
+    public GameObject RepairKit;
+    public GameObject SingleAmmo;
+    public GameObject TwentyPackAmmo;
+    public GameObject ShieldTwentyPercent;
+
+    [Header("Ammo, Shield, Repair Kit Count")]
+    private float m_maxBonusShield = 2.5f;
+    private int m_maxAmmo = 30;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddShield(float shieldValueAmount)
+    {
+        Debug.Log("Will be adding an event to activate");
+
+
+    }
+
+    private void DoShieldIncrease()
+    {
+
+    }
+}
